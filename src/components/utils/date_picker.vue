77dc3e8118111ec7f135141015e07f1a9fd898ba
lennyjuma <lennyndungu34@gmail.com>
2024-08-26 11:26:16 +0300

    added range date

--- src/components/utils/date_picker.vue
+++ src/components/utils/date_picker.vue
@@ -1,11 +1,18 @@
 <template>
-  <VueDatePicker v-model="date"></VueDatePicker>
+  <VueDatePicker v-model="date" range></VueDatePicker>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted  } from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const date = ref();
+
+// For demo purposes assign range from the current date
+onMounted(() => {
+  const startDate = new Date();
+  const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
+  date.value = [startDate, endDate];
+})
 </script>
