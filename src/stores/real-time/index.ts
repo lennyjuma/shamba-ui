7afad77886bfc284087181012d56c484fdea5746
lennyjuma <lennyndungu34@gmail.com>
2024-08-28 13:58:24 +0300

    added real time store

--- src/stores/real-time/index.ts
+++ src/stores/real-time/index.ts
@@ -1,16 +1,33 @@
-import { ref, computed } from 'vue'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { airT, soilT } from '@/types'
+import router from '@/router'
+import { useRestController } from '@/compossables/Axios'
 
 export const useRealTimeStore = defineStore('real_time', () => {
+  const soil_path = "soil/latest"
+  const air_path = "air/latest"
   const air = ref<airT>({  } as airT)
   const soil = ref<soilT>({  } as soilT)
   const getAir = computed(() => air.value )
   const getSoil = computed(() => soil.value )
   function fetchLatestAir() {
+    const deviceId = `${router.currentRoute.value.query["deviceId"]}`;
+    const url = `${air_path}?device_id=${deviceId}`;
+    useRestController(url, "get", {}).then(({ responseDTO }) => {
+      // @ts-ignore
+      air.value= responseDTO.value.data;
+    });
 
   }
   function fetchLatestSoil() {
+    const deviceId = `${router.currentRoute.value.query["deviceId"]}`;
+    const url = `${soil_path}?device_id=${deviceId}`;
+    useRestController(url, "get", {}).then(({ responseDTO }) => {
+      // @ts-ignore
+      soil.value= responseDTO.value.data;
+    });
+
 
   }
 
