453c8d5bb35ac4497960c8b7936a3e097e605bfe
lennyjuma <lennyndungu34@gmail.com>
2024-08-28 11:26:33 +0300

    air properties table done

--- src/views/tables/air_table.vue
+++ src/views/tables/air_table.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="px-4 sm:px-6 lg:px-8 max-w-9xl mx-auto">
 
-    <div class="-mx-4 mt-8 sm:-mx-0">
+    <div class="-mx-4 mt-4 mb-3 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
       <table class="min-w-full divide-y divide-gray-300">
         <thead>
-        <tr>
-          <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Temperature</th>
+        <tr class="mx-2">
+          <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-2">Temperature</th>
           <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Humidity</th>
           <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Crop</th>
           <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Date</th>
@@ -13,19 +13,14 @@
         </tr>
         </thead>
         <tbody class="divide-y divide-gray-200 bg-white">
-        <tr v-for="person in people" :key="person.email">
-          <td class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-            {{ person.name }}
-            <dl class="font-normal lg:hidden">
-              <dt class="sr-only">Title</dt>
-              <dd class="mt-1 truncate text-gray-700">{{ person.title }}</dd>
-              <dt class="sr-only sm:hidden">Email</dt>
-              <dd class="mt-1 truncate text-gray-500 sm:hidden">{{ person.email }}</dd>
-            </dl>
+        <tr v-for="air in air_properties" :key="air.id">
+          <td class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-4">
+            {{ air.temperature }}
+
           </td>
-          <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ person.title }}</td>
-          <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ person.email }}</td>
-          <td class="px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
+          <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ air.humidity }}</td>
+          <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ air.crop }}</td>
+          <td class="px-3 py-4 text-sm text-gray-500">{{ air.reading_date }}</td>
           <td class="border-t border-transparent', 'relative py-3.5 pl-3 pr-4 text-left font-medium sm:pr-6">
             <button @click="go_to_maps()" type="button" class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white" >
               Go to map
@@ -36,17 +31,38 @@
         </tbody>
       </table>
     </div>
+
+    <pagination :pagination-data="getPagination" @fetchTransaction="(p)=> changePagewithPagination(p)"  class="my-2" />
   </div>
 </template>
 
-<script setup>
-const people = [
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-  // More people...
-]
+<script setup lang="ts">
+
+import { useAirStore } from '@/stores/air'
+import { storeToRefs } from 'pinia'
+import { onMounted, ref, watch } from 'vue'
+import type { airT } from '@/types'
+import Pagination from '@/components/navigation/pagination.vue'
+
+let airStore = useAirStore()
+const {fetchAirByDeviceId,changePage} =  airStore
+const {get_air,getPagination} =  storeToRefs(airStore)
+
+const page_size = 10;
+const air_properties = ref<airT[]>([] as airT[])
 
 const go_to_maps = () => {
   window.open('https://maps.google.com/?q=-1.181423908681221, 36.935804866892255', '_blank');
 
 }
+const changePagewithPagination = (param: number) => {
+  changePage(param,page_size);
+};
+
+watch(get_air,() =>{
+  air_properties.value =get_air.value
+})
+onMounted(()=>{
+  fetchAirByDeviceId(0,10)
+})
 </script>
